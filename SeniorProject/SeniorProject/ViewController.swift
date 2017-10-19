01be58e8faa17c724789d65329d53b439d6bfa56
Carolyn Beranek <carolynberanek@Carolyns-MacBook-Pro.local>
2017-10-19 14:05:14 -0400

    added code to start core data

--- SeniorProject/SeniorProject/ViewController.swift
+++ SeniorProject/SeniorProject/ViewController.swift
@@ -12,11 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var newEntryButton: UIButton!
     @IBOutlet weak var pastEntriesList: UITableView!
+    var entries: [String] = []
     
+    
+    
+    @IBAction func addEntry(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "Past Entries"
+        pastEntriesList.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +33,24 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                              for: indexPath)
+            cell.textLabel?.text = entries[indexPath.row]
+            return cell
+    }
 }
 
