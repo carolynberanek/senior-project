5e3380b74d3e6b1d00d2852ad9c0439764a3ad9e
Harry Baird <hbaird3@users.noreply.github.com>
2017-10-20 12:57:17 -0400

    Remove title from view controller

--- SeniorProject/SeniorProject/ViewController.swift
+++ SeniorProject/SeniorProject/ViewController.swift
@@ -21,8 +21,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        title = "Past Entries"
         pastEntriesList.register(UITableViewCell.self,
                            forCellReuseIdentifier: "Cell")
     }
